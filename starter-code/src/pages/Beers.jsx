a2c3b402bcad15bc526d302450ed429c5b561e17
juliacanas <julia_cl90@hotmail.com>
2020-04-19 22:31:07 +0200

    finished

--- starter-code/src/pages/Beers.jsx
+++ starter-code/src/pages/Beers.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import styles from '../App.module.css'
 
 import { BeerCard } from '../components/beers/BeerCard';
 
@@ -10,9 +11,17 @@ export const Beers = () => {
 		axios.get('https://ih-beers-api2.herokuapp.com/beers')
 			.then((res) => setBeers(res.data));
   }, []);
+
+  const searchBeer = (event) => {
+		const {value} = event.target;
+
+		axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${value}`)
+			.then( (res) => setBeers(res.data));
+	};
   
   return (
     <div>
+      <input className={styles.search} type='text' onChange={searchBeer} placeholder='Search...'/>
       {beers.map((beer) =>
 				<BeerCard key={beer._id} beer={beer}/>
 			)}
